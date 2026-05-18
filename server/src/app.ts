7bb4f9a4f0355a655fb567cdc890e675326f8bc0
Rubul-Tanti <rubultanti404@gmail.com>
2026-05-18 13:42:37 +0530

    added a winston logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,6 @@ import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
 // import { prisma } from './db/prisma'
 dotenv.config()
-
 export const app=express()
 app.use(helmet())
 app.use(express.json())
